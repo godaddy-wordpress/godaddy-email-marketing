694e411e64eff6e70d15c23a7dbf3c6c0dc0bcea
Maor Chasen <maor@illuminea.com>
2013-05-09 17:22:53 +0300

    template tag added

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -13,3 +13,17 @@ class Mad_Mimi_Shortcode {
 		Mad_Mimi_Form_Renderer::process( $id );
 	}
 }
+
+/**
+ * The main template tag. Pass on the ID and watch the magic happen.
+ * 
+ * @since 1.0
+ * @see Mad_Mimi_Form_Renderer
+ * @param int $id The ID of the form you wish to output
+ */
+function madmimi_form( $id ) {
+	if ( ! class_exists( 'Mad_Mimi_Form_Renderer' ) )
+		return;
+
+	Mad_Mimi_Form_Renderer::process( $id );
+}
