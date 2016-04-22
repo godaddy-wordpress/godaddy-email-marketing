e664d8b7dc975a43c7e8215a47e4e216b4be8eb2
Derek Herman <derek@valendesigns.com>
2016-04-22 03:24:55 -0700

    Remove thickbox resize JS

--- js/admin.js
+++ js/admin.js
@@ -9,33 +9,11 @@
 
 	var GEMAdmin = {
 
-		cache: {},
-
 		init: function() {
-			this.cacheElements();
-			this.bindEvents();
-		},
-
-		cacheElements: function() {
-			this.cache = {
-				$window: $( window ),
-				$document: $( document )
-			};
-		},
-
-		bindEvents: function() {
 			var self = this;
 
-			self.cache.$window.on( 'resize', $.proxy( self.tbPosition, self ) );
-
-			self.cache.$document.on( 'ready', function() {
+			$( document ).on( 'ready', function() {
 				self.tabbedNav();
-
-				$( '.about-wrap' ).on( 'click', 'a.thickbox', function() {
-					tb_click.call( this );
-					self.cache.$window.trigger( 'resize' );
-					return false;
-				} );
 			} );
 		},
 
@@ -46,7 +24,7 @@
 			// Hide all panels
 			$( 'div.panel', $wrap ).hide();
 
-			this.cache.$window.on( 'load', function() {
+			$( window ).on( 'load', function() {
 				var tab = self.getParameterByName( 'tab' ),
 					hashTab = window.location.hash.substr( 1 );
 
@@ -83,44 +61,6 @@
 			regex = new RegExp( '[\\?&]' + name + '=([^&#]*)' );
 			results = regex.exec( location.search );
 			return null === results ? '' : decodeURIComponent( results[1].replace( /\+/g, ' ' ) );
-		},
-
-		tbPosition: function() {
-			var $tbWindow = $( '#TB_window' ),
-				$tbFrame = $( '#TB_iframeContent' ),
-				windowWidth = this.cache.$window.width(),
-				newHeight = this.cache.$window.height() - ( ( 792 < windowWidth ) ? 90 : 50 ),
-				newWidth = ( 792 < windowWidth ) ? 772 : windowWidth - 20;
-
-			if ( $tbWindow.size() ) {
-				$tbWindow
-					.width( newWidth )
-					.height( newHeight )
-					.css( { 'margin-left': '-' + parseInt( ( newWidth / 2 ), 10 ) + 'px' } );
-
-				$tbFrame.width( newWidth ).height( newHeight );
-
-				if ( 'undefined' !== typeof document.body.style.maxWidth ) {
-					$tbWindow.css( {
-						'top': ( 792 < windowWidth ? 30 : 10 ) + 'px',
-						'margin-top': '0'
-					} );
-				}
-			}
-
-			return $( 'a.thickbox' ).each( function() {
-				var href = $( this ).attr( 'href' );
-
-				if ( ! href ) {
-					return;
-				}
-
-				href = href.replace( /&width=[0-9]+/g, '' );
-				href = href.replace( /&height=[0-9]+/g, '' );
-				href = href + '&width=' + newWidth + '&height=' + newHeight;
-
-				$( this ).attr( 'href', href );
-			} );
 		}
 
 	};
