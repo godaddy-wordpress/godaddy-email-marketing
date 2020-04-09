56dc8a9356e793e3132f7bb14278bc4b7f3d50f0
AnthonyLedesma <anthonymledesma@gmail.com>
2020-04-09 13:55:08 -0700

    updated for 5.4

--- godaddy-email-marketing.php
+++ godaddy-email-marketing.php
@@ -3,7 +3,7 @@
  * Plugin Name: GoDaddy Email Marketing
  * Plugin URI: https://gem.godaddy.com/
  * Description: Add the GoDaddy Email Marketing signup form to your WordPress site! Easy to set up, the plugin allows your site visitors to subscribe to your email lists.
- * Version: 1.4.2
+ * Version: 1.4.3
  * Author: GoDaddy
  * Author URI: https://gem.godaddy.com/
  * License: GPL-2.0
@@ -109,7 +109,7 @@ class GEM_Official {
 
 		// Plugin's main directory.
 		defined( 'GEM_VERSION' )
-			or define( 'GEM_VERSION', '1.4.2' );
+			or define( 'GEM_VERSION', '1.4.3' );
 
 		// Set up the base name.
 		self::$basename = isset( self::$basename ) ? self::$basename : plugin_basename( __FILE__ );
